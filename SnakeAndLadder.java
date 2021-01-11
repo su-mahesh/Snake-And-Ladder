06659459bbb0d9ca113d811c8a842d79d11daa7b
su-mahesh <maheshkangude@gmail.com>
2021-01-11 20:57:56 +0530

    single player start position 0

---
+++ SnakeAndLadder.java
@@ -0,0 +1,17 @@
+class SnakeAndLadder
+{
+	public static void main(String[] args)
+	{
+	//constans
+	final int start_position = 0;
+
+	//variable
+	int playerPosition = 0;
+	int dice_roll_num = 0;
+	int player = 1;
+	//computation
+	playerPosition = start_position;
+	System.out.println("player: "+ player +" Player Position: "+ playerPosition );
+	}
+
+}
