3666e6431eb5ac1280fecf01d0005748661b249e
su-mahesh <maheshkangude@gmail.com>
2021-01-11 23:47:45 +0530

    player plas untill wins position 100

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -5,9 +5,13 @@ class Player
 	
 	//functions
 	void setPosition(int position)
-	{
-	playerPosition = position;
+	{	
+		
+	playerPosition += position;
+	if (playerPosition < 0)
+	playerPosition = 0;
 	}
+	
 	int getPosition()
 	{
 	return	playerPosition;
@@ -20,10 +24,12 @@ class SnakeAndLadder
 {
 	//variable
 	static int diceOutcome = 0;
+	
 	Player p;
 	
 	//constanst
 	static final int start_position = 0;
+	static final int winning_position = 100;
 		
 	static enum play_Option{
 	NoPlay,
@@ -61,6 +67,7 @@ class SnakeAndLadder
 		return 0;	
 }
 	}
+	
 	//Main funtion
 	public static void main(String[] args)
 	{
@@ -68,19 +75,26 @@ class SnakeAndLadder
 	Player player[] = new Player[1];	
 	player[0] = new Player();
 	play_Option nextMove = play_Option.NoPlay;	
+	int playerPreviousPosition = 0;
 	
 	//computation
 	initialiseGame(player[0]);
 	
+while(player[0].getPosition() != winning_position)
+{	
 	diceOutcome = rollDice();
 	
 	nextMove = checkPlayOption();
 	
+	playerPreviousPosition = player[0].getPosition();
+	
 	player[0].setPosition(getMoves( nextMove,	diceOutcome ));
-
 	System.out.println("Dice rolled, Outcome: " + diceOutcome);
-	System.out.println("play option: " + nextMove);	
- 	System.out.println("Player Position: "+ player[0].getPosition() );
+	System.out.println("play option: " + nextMove);
+	System.out.println("Player Position: "+ playerPreviousPosition+" >> "+player[0].getPosition() + "\n");
+
+}
+	
 
 	
 	}
