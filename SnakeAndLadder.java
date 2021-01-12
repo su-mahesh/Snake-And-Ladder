413cdf5ca05ac450ae6a10d66462f99d69122f7a
su-mahesh <maheshkangude@gmail.com>
2021-01-12 20:29:08 +0530

    refctored

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,8 +3,19 @@ class Player
 	//variable
 	private int playerPosition = 0;
 	private int diceRollCount = 0;
+	private String playerName = null; 
 	
 	//functions
+	String getPlayerName()
+	{
+	return playerName;
+	}
+
+	Player(String playerName)
+	{
+	this.playerName = playerName;
+	}
+
 	void setPosition(int position)
 	{	
 	
@@ -32,40 +43,31 @@ class Player
 class SnakeAndLadder
 {
 	//variable
-	static int diceOutcome = 0;
-	
-	Player p;
+	int diceOutcome = 0;
+	static int winning_player = 0;
 	
 	//constanst
-	static final int start_position = 0;
+	final int start_position = 0;
 	static final int winning_position = 100;
 		
-	static enum play_Option{
+	enum play_Option{
 	NoPlay,
 	Ladder,
    Snake
 	}
 	
-	static play_Option checkPlayOption() {
-
+	play_Option checkPlayOption() {
 	return play_Option.values()[(int) (Math.random() * 10) %3];
-
-
 	}
 
-	static int rollDice()
+	int rollDice()
 	{
 	return (int) (Math.random() * 10) %6 + 1;
-	
 	}
 	
-	static void initialiseGame(	Player p	)
+	int getMoves(play_Option nextMove, int diceOutcome){
+	switch(nextMove)
 	{
-	p.setPosition(	start_position	);
-	}
-	static int getMoves(play_Option nextMove, int diceOutcome){
-			switch(nextMove)
-{
 		case Ladder:	
 		return diceOutcome;
 					
@@ -74,60 +76,69 @@ class SnakeAndLadder
 					
 		default: 
 		return 0;	
-}
+	}
 	}
 	
-	static int toss()
+	int toss()
 	{
 		return (int) (Math.random() * 10) %2;
 	}
 	
-	//Main funtion
-	public static void main(String[] args)
+	void playGame(Player player[])
 	{
-	
-	Player player[] = new Player[2];	
-	player[0] = new Player();
-	player[1] = new Player();
+	player[0].setPosition(start_position);
+	player[1].setPosition(start_position);
 	
 	play_Option nextMove = play_Option.NoPlay;	
 	int playerPreviousPosition = 0;
 	int currentPlayer = 0;
 	int idlePlayer = 1;
 	
-	//computation
-	initialiseGame(player[0]);
-	initialiseGame(player[1]);
-	
-	if(	toss()	==	0 )
+	if( toss()	==	0 )
 	{
 	currentPlayer = 1;
 	idlePlayer = 0;
 	}	
 	
-while(	player[0].getPosition() != winning_position && player[1].getPosition() != winning_position	)
-{	
+	System.out.println(player[currentPlayer].getPlayerName()+ " won the toss \n");
+	
+	while(	player[0].getPosition() != winning_position && player[1].getPosition() != winning_position	)
+	{
 	diceOutcome = rollDice();
 	player[	currentPlayer	].countDiceRoll();
 	nextMove = checkPlayOption();
 	
 	playerPreviousPosition = player[currentPlayer].getPosition();
 	
-	player[	currentPlayer	].setPosition(getMoves( nextMove,	diceOutcome ));
-	System.out.println("Player "+(currentPlayer	+	1)+" playing");
+	player[	currentPlayer	].setPosition(getMoves( nextMove, diceOutcome ));
+	System.out.println("Player '"+player[currentPlayer].getPlayerName()+"' is playing");
 	System.out.println("Dice roll No: " +	player[currentPlayer].getDiceRollCount()	+ " Outcome: " + diceOutcome);
 	System.out.println("play option: " + nextMove);
 	System.out.println("Player Position: "+ playerPreviousPosition+" >> "+player[currentPlayer].getPosition() + "\n");
 
 	if(nextMove	!=	play_Option.Ladder)
 	{
-	
 	int temp = idlePlayer;
 	idlePlayer = currentPlayer;
 	currentPlayer = temp;
 	}
-}
-	System.out.println("Player "+(currentPlayer	+	1)+" won");
+	}
+	winning_player = currentPlayer; 
+	}
+	
+	//Main funtion
+	public static void main(String[] args)
+	{
+	Player player[] = new Player[2];	
+	player[0] = new Player("Jack");
+	player[1] = new Player("Jill");
+	
+	SnakeAndLadder	SnakeAndLadderGame = new SnakeAndLadder();
+
+	//computation	
+	SnakeAndLadderGame.playGame(player);
+		
+	System.out.println("Player "+	player[winning_player].getPlayerName()	+" won");
 	
 	}
 
